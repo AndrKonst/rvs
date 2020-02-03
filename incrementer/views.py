3ab96397f4ed6596dee21284ff97940ebec36967
AndrKonst <Andr_k.93@mail.ru>
2020-02-03 13:16:43 +0300

    added html template

--- incrementer/views.py
+++ incrementer/views.py
@@ -1,25 +1,35 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Num, Error_log
 
 
 def increment(request, num):
+    template = loader.get_template('incrementer/increment.html')
     try:
         num = int(num)
         for used_num in Num.objects.all():
             if num == used_num.num:
                 er = Error_log(err='Error_1', number=used_num)
                 er.save()
-                return HttpResponse('Число {} уже обрабатывалось ранее'.format(num))
+                output_str = 'Число {} уже обрабатывалось ранее'.format(num)
+                context = {'output_str': output_str}
+                return HttpResponse(template.render(context, request))
             elif num + 1 == used_num.num:
                 er = Error_log(err='Error_2', number=used_num)
                 er.save()
-                return HttpResponse('Число {} на 1 меньше обработанного ранее числа'.format(num))
+                output_str = 'Число {} на 1 меньше обработанного ранее числа'.format(num)
+                context = {'output_str': output_str}
+                return HttpResponse(template.render(context, request))
 
         used_num = Num(num=num)
         used_num.save()
-        return HttpResponse('Обработанное число = {}'.format(num + 1))
+        output_str = 'Обработанное число = {}'.format(num + 1)
+        context = {'output_str': output_str}
+        return HttpResponse(template.render(context, request))
     except ValueError:
-        return HttpResponse('Запрос не натуральное число!!!')
+        output_str = 'Запрос не натуральное число!!!'
+        context = {'output_str': output_str}
+        return HttpResponse(template.render(context, request))
 
 
 def clear_db(request):
