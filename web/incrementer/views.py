d044af8425dcc5d4fe25e560dd35009ec0d4bfcf
AndrKonst <Andr_k.93@mail.ru>
2020-02-29 12:43:25 +0300

    db connection in app + curl request

--- web/incrementer/views.py
+++ web/incrementer/views.py
@@ -1,62 +1,84 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.db.models import F
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .forms import InputNum_Form
-from .models import Num, Error_log
+from .models import Num
 
 import psycopg2 as sql
 import os, json
 
-def increment(request):
-    if request.method == 'POST':
-        nf = InputNum_Form(request.POST)
-        if nf.is_valid():
-            num = nf.cleaned_data['input_num_form']
-            if num < 0:
-                output_str = 'Число {} меньше 0. Введите натуральное число'.format(num)
-                return render(request, 'incrementer/increment.html', {'output_str': output_str, 'form': nf})
-            num_from_db = Num.objects.values('num', num_minus_1=F('num') - 1)
-            if num_from_db.exists():
-                for used_num in num_from_db:
-                    if used_num['num'] == num:
-                        er = Error_log(err='Error_1', number=Num.objects.get(num=num))
-                        er.save()
-                        output_str = 'Число {} уже обрабатывалось ранее'.format(num)
-                        return render(request, 'incrementer/increment.html', {'output_str': output_str, 'form': nf})
-                    elif used_num['num_minus_1'] == num:
-                        er = Error_log(err='Error_2', number=Num.objects.get(num=num + 1))
-                        er.save()
-                        output_str = 'Число {} на 1 меньше обработанного ранее числа'.format(num)
-                        return render(request, 'incrementer/increment.html', {'output_str': output_str, 'form': nf})
-            used_num = Num(num=num)
-            used_num.save()
-            output_str = 'Обработанное число = {}'.format(num + 1)
-            return render(request, 'incrementer/increment.html', {'output_str': output_str, 'form': nf})
-    else:
-        nf = InputNum_Form()
-        return render(request, 'incrementer/increment.html', {'form': nf})
-
 @csrf_exempt
 def curl(request):
-    conn = sql.connect(dbname=os.environ.get('RVS_DB_NAME'),
-                       user=os.environ.get('RVS_DB_USER'),
-                       password=os.environ.get('RVS_DB_PASS'),
-                       host=os.environ.get('RVS_DB_HOST'),
-                       port=os.environ.get('RVS_DB_PORT'))
-    cur = conn.cursor()
+    # http method check
     if request.method == 'POST':
         in_data = json.loads(request.body)
-        cur.close()
-        conn.close()
-        return HttpResponse('got Post\n')
+
+        # number check
+        for i in in_data.values():
+            if type(i) != int:
+                ans = {'msg': 'Wrong type of input data'}
+                return HttpResponse(JsonResponse(ans))
+            if i < 0:
+                ans = {'msg': 'input number must be positive'}
+                return HttpResponse(JsonResponse(ans))
+
+        # db connection
+        try:
+            conn = sql.connect(dbname=os.environ.get('RVS_DB_NAME'),
+                               user=os.environ.get('RVS_DB_USER'),
+                               password=os.environ.get('RVS_DB_PASS'),
+                               host=os.environ.get('RVS_DB_HOST'),
+                               port=os.environ.get('RVS_DB_PORT'))
+        except sql.OperationalError as err:
+            ans = {'msg': 'Connection db error: {}'.format(err)}
+            return HttpResponse(JsonResponse(ans))
+        cur = conn.cursor()
+
+        # creation tables
+        cur.execute('CREATE TABLE IF NOT EXISTS nums (num INTEGER PRIMARY KEY)')
+        conn.commit()
+
+        cur.execute('CREATE TABLE IF NOT EXISTS incrementer_conflicts \
+                    (id SERIAL PRIMARY KEY,\
+                     conflict_type INTEGER NOT NULL,\
+                     number INTEGER REFERENCES nums (num) ON DELETE CASCADE,\
+                     date_time TIMESTAMP )')
+        conn.commit()
+
+        # check for conflicts
+        for num in in_data.values():
+            cur.execute('SELECT num, num-1 FROM nums WHERE num=%s', (num,))
+            db_nums = cur.fetchone()
+            if not db_nums:
+                cur.execute('INSERT INTO nums VALUES (%s)', (num,))
+                conn.commit()
+                cur.close()
+                conn.close()
+                ans = {'msg': 'Number {} processed'.format(num)}
+                return HttpResponse(JsonResponse(ans))
+            elif num == db_nums[0]:
+                cur.execute('INSERT INTO incrementer_conflicts (conflict_type, number, date_time) \
+                             VALUES (1, %s, now())', (num,))
+                conn.commit()
+                cur.close()
+                conn.close()
+                ans = {'msg': 'Conflict 1. Number {} has already been processed'.format(num,)}
+                return HttpResponse(JsonResponse(ans))
+            elif num == db_nums[1]:
+                cur.execute('INSERT INTO incrementer_conflicts (conflict_type, number, date_time) \
+                             VALUES (2, %s, now())', (num,))
+                conn.commit()
+                cur.close()
+                conn.close()
+                ans = {'msg': 'Conflict 2. Number {} is 1 less then the previously processed number'.format(num,)}
+                return HttpResponse(JsonResponse(ans))
+
+    # actions if method not post
     else:
-        cur.close()
-        conn.close()
-        return HttpResponse('not got POST\n')
+        ans = {'msg': 'HTTP method not POST'}
+        return HttpResponse(JsonResponse(ans))
 
 
 def clear_db(request):
     for t in Num.objects.all():
         t.delete()
-    return HttpResponse('Записи в базе данных удалены')
+    ans = {'msg': 'Db cleared'}
+    return HttpResponse(JsonResponse(ans))
